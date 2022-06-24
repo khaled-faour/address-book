4c8ed40421bbd2e25da83930ce59a493f3ab3107
Khaled Faour <khaledfaour2@gmail.com>
2022-06-25 00:01:04 +0300

    Added Contact Controller Functions

--- controller/contact.js
+++ controller/contact.js
@@ -1,31 +1,83 @@
-const Conatct = require('../model/Contact');
+const Contact = require('../model/Contact');
+
+const addContact = async (req, res) =>{
+    try{
+        const { user,
+            first_name,
+            last_name,
+            email,
+            phone,
+            relation,
+            location} = req.body;
+    
+            const contact = await Contact.create({
+                user,
+                first_name,
+                last_name,
+                email: email.toLowerCase(),
+                phone,
+                relation,
+                location
+            });
+            return res.status(200).json({contact});
+    }catch(err){
+        res.status(500).json({error: err})
+    }
+}
 
 const getContacts = async (req, res) =>{
-    res.status(200).json({
-        message: "GET"
-    })
+    try{
+        const contacts = await Contact.find().populate('user');
+        return res.status(200).json({contacts});
+    }catch(err){
+        res.status(500).json({error: err})
+    }
 }
 
 const getContact = async (req, res) =>{
-    const {id} = req.params
-    res.status(200).json({
-        message: "GET"
-    })
+    try{
+        const {id} = req.params;
+        const contact = await Contact.findById(id).populate('user');
+        return res.status(200).json({contact});
+    }catch(err){
+        res.status(500).json({error: err})
+    }
 }
-const updateContact = async (req, res) =>{
-    const {id} = req.params
-    res.status(200).json({
-        message: "GET"
-    })
+const updateContact = async (req, res) => {
+    try{
+        const {id} = req.params
+        const { first_name,
+            last_name,
+            email,
+            phone,
+            relation,
+            location } = req.body;
+    
+            const contact = await Contact.findByIdAndUpdate(id, {
+                first_name,
+                last_name,
+                email: email.toLowerCase(),
+                phone,
+                relation,
+                location
+            });
+            return res.status(200).json({contact});
+    }catch(err){
+        res.status(500).json({error: err})
+    }
 }
 const deleteContact = async (req, res) =>{
-    const {id} = req.params
-    res.status(200).json({
-        message: "GET"
-    })
+    try{
+        const {id} = req.params;
+        const contact = await Contact.findByIdAndDelete(id);
+        return res.status(200).json({contact});
+    }catch(err){
+        res.status(500).json({error: err})
+    }
 }
 
 module.exports ={
+    addContact,
     getContact,
     getContacts,
     updateContact,
